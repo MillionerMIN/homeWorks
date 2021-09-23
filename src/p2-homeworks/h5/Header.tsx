49ca1965a930ada1fa54b20707bb795030d5884c
Vladimir Liankevich <vladimirlyn@gmail.com>
2021-09-23 22:59:05 +0300

    animation nav bar

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,40 +1,50 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import s from './Header.module.css';
-import { FaArrowAltCircleLeft} from 'react-icons/fa';
+import { FaArrowAltCircleLeft } from 'react-icons/fa';
 
 function Header() {
-    const [active, setActive] = useState<boolean>(false)
-    const toggle = () => {
-        if (active) {
-            setActive(false)
-        } else {
-            setActive(true)
-        }
-    }
-    return (
-        <>
-            <div
-                onClick={() => toggle()}
-            >
-                <div className={active ? s.container : s.container_none}>
-                    <NavLink to="/pre-junior"
-                        className={s.link}
-                        activeClassName={s.active}
-                    >PreJunior</NavLink>
-                    <NavLink to="/junior"
-                        className={s.link}
-                        activeClassName={s.active}
-                    >Junior</NavLink >
-                    <NavLink to="/juniorPlus"
-                        className={s.link}
-                        activeClassName={s.active}
-                    >Junior</NavLink >
-                </div >
-                <span><FaArrowAltCircleLeft className={!active ? s.turn : s.turn_180}/></span>
-            </div>
-        </>
-    )
+  const [open, setOpen] = useState<boolean>(true);
+  const close = () => setOpen(false);
+  return (
+    <>
+      <nav className={open ? `${s.menu} ${s.nav} ` : `${s.none} ${s.nav}`}>
+        <div className={s.wrapper}>
+          <NavLink
+            to="/pre-junior"
+            className={s.link}
+            activeClassName={s.active}
+            onClick={close}
+          >
+            PreJunior
+          </NavLink>
+          <NavLink
+            to="/junior"
+            className={s.link}
+            activeClassName={s.active}
+            onClick={close}
+          >
+            Junior
+          </NavLink>
+          <NavLink
+            to="/juniorPlus"
+            className={s.link}
+            activeClassName={s.active}
+            onClick={close}
+          >
+            Junior
+          </NavLink>
+        </div>
+
+        <FaArrowAltCircleLeft
+          onClick={() => {
+            setOpen(!open);
+          }}
+          className={!open ? s.turn : s.turn_180}
+        />
+      </nav>
+    </>
+  );
 }
 
-export default Header
+export default Header;
